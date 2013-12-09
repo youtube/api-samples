7b1e27325430ac4976ce85d0ee2a356d7173cf96
Andy Diamondstein <adiamondstein@google.com>
2013-12-09 10:52:24 -0800

    "Added sample: javascript/search.js"

---
+++ javascript/search.js
@@ -0,0 +1,18 @@
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
+}
+
+// Search for a specified string.
+function search() {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#search-container').html('<pre>' + str + '</pre>');
+  });
+}
