8a1a9e4df4f93e3f9eeee44419e54461b8425d5d
Andy Diamondstein <adiamondstein@google.com>
2013-12-09 10:52:25 -0800

    "Added sample: php/upload_thumbnail.php"

---
+++ php/upload_thumbnail.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This sample uploads and sets a custom thumbnail for a video.
+ *
+ * 1. It uploads an image using the "Google_MediaFileUpload" class.
+ * 2. It sets the uploaded image as a custom thumbnail to the video by
+ *    calling the API's "youtube.thumbnails.set" method
+ *
+ * @author Ibrahim Ulukaya
+*/
+
+
+// Call set_include_path() as needed to point to your client library.
+require_once 'Google_Client.php';
+require_once 'contrib/Google_YouTubeService.php';
+session_start();
+
+/*
+ * You can acquire an OAuth 2.0 client ID and client secret from the
+ * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
+ * For more information about using OAuth 2.0 to access Google APIs, please see:
+ * <https://developers.google.com/youtube/v3/guides/authentication>
+ * Please ensure that you have enabled the YouTube Data API for your project.
+ */
+$OAUTH2_CLIENT_ID = 'REPLACE_ME';
+$OAUTH2_CLIENT_SECRET = 'REPLACE_ME';
+
+$client = new Google_Client();
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+    FILTER_SANITIZE_URL);
+$client->setRedirectUri($redirect);
+
+// Define an object that will be used to make all API requests.
+$youtube = new Google_YoutubeService($client);
+
+if (isset($_GET['code'])) {
+  if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+    die('The session state did not match.');
+  }
+
+  $client->authenticate();
+  $_SESSION['token'] = $client->getAccessToken();
+  header('Location: ' . $redirect);
+}
+
+if (isset($_SESSION['token'])) {
+  $client->setAccessToken($_SESSION['token']);
+}
+
+// Check to ensure that the access token was successfully acquired.
+if ($client->getAccessToken()) {
+  try{
+
+    // REPLACE this value with the video ID of the video being updated.
+    $videoId = "VIDEO_ID";
+
+    // REPLACE this value with the path to the image file you are uploading.
+    $imagePath = "/path/to/file.png";
+
+    // Specify the size of each chunk of data, in bytes. Set a higher value for
+    // reliable connection as fewer chunks lead to faster uploads. Set a lower
+    // value for better recovery on less reliable connections.
+    $chunkSizeBytes = 1 * 1024 * 1024;
+
+    // Create a MediaFileUpload object for resumable uploads.
+    $media = new Google_MediaFileUpload('image/png', null, true, $chunkSizeBytes);
+    $media->setFileSize(filesize($imagePath));
+
+    // Call the API's thumbnails.set method to upload the image and associate
+    // it with the appropriate video.
+    $setResponse = $youtube->thumbnails->set($videoId, array('mediaUpload' => $media));
+
+    $uploadStatus = false;
+
+    // Read the image file and upload it chunk by chunk.
+    $handle = fopen($imagePath, "rb");
+    while (!$uploadStatus && !feof($handle)) {
+      $chunk = fread($handle, $chunkSizeBytes);
+      $uploadStatus = $media->nextChunk($setResponse, $chunk);
+    }
+
+    fclose($handle);
+
+    $thumbnailUrl = $uploadStatus['items'][0]['default']['url'];
+    $htmlBody .= "<h3>Thumbnail Uploaded</h3><ul>";
+    $htmlBody .= sprintf('<li>%s (%s)</li>',
+        $videoId,
+        $thumbnailUrl);
+    $htmlBody .= sprintf('<img src="%s">', $thumbnailUrl);
+    $htmlBody .= '</ul>';
+
+
+    } catch (Google_ServiceException $e) {
+      $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+          htmlspecialchars($e->getMessage()));
+    } catch (Google_Exception $e) {
+      $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+          htmlspecialchars($e->getMessage()));
+    }
+
+    $_SESSION['token'] = $client->getAccessToken();
+    } else {
+      // If the user hasn't authorized the app, initiate the OAuth flow
+      $state = mt_rand();
+      $client->setState($state);
+      $_SESSION['state'] = $state;
+
+      $authUrl = $client->createAuthUrl();
+      $htmlBody = <<<END
+  <h3>Authorization Required</h3>
+  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+END;
+    }
+    ?>
+
+    <!doctype html>
+    <html>
+    <head>
+    <title>Claim Uploaded</title>
+    </head>
+    <body>
+      <?=$htmlBody?>
+    </body>
+    </html>
