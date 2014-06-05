294e20fdc3ba31ed431502da5864ae1ddab053e4
Ibrahim Ulukaya <ulukaya@google.com>
2014-06-05 15:26:14 -0400

    "Added sample: python/unset_watermark.py"

---
+++ python/unset_watermark.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+# Usage example:
+# python unset_watermark.py --channelId='<channel_id>' --unset
+
+import json
+import os
+import sys
+import httplib2
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.file import Storage
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.tools import run
+from optparse import OptionParser
+
+
+# CLIENT_SECRETS_FILE, name of a file containing the OAuth 2.0 information for
+# this application, including client_id and client_secret. You can acquire an
+# ID/secret pair from the API Access tab on the Google APIs Console
+#   http://code.google.com/apis/console#access
+# For more information about using OAuth2 to access Google APIs, please visit:
+#   https://developers.google.com/accounts/docs/OAuth2
+# For more information about the client_secrets.json file format, please visit:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+# Please ensure that you have enabled the YouTube Data API for your project.
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# An OAuth 2 access scope that allows for full read/write access.
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# Helpful message to display if the CLIENT_SECRETS_FILE is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+   %s
+with information from the APIs Console
+https://developers.google.com/console
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+# Authorize the request and store authorization credentials.
+def get_authenticated_service():
+  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+    scope=YOUTUBE_READ_WRITE_SCOPE,
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+  storage = Storage("%s-oauth2.json" % sys.argv[0])
+  credentials = storage.get()
+
+  if credentials is None or credentials.invalid:
+    credentials = run(flow, storage)
+
+  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    http=credentials.authorize(httplib2.Http()))
+
+
+# Call the API's watermarks.unset method to remove the watermark image.
+def unset_watermark(youtube, channel_id):
+  try:
+    youtube.watermarks().unset(channelId=channel_id).execute()
+  except HttpError as e:
+    print "Error while unsetting watermark: %s" % e.content
+    raise e
+
+
+if __name__ == "__main__":
+  parser = OptionParser()
+  parser.add_option("--channelid", dest="channelid",
+    help="Required; id of channel whose watermark you're updating.")
+  (options, args) = parser.parse_args()
+  
+  if not options.channelid:
+    parser.print_help()
+    exit()
+
+  youtube = get_authenticated_service()
+
+  unset_watermark(youtube, options.channelid)
+  print "The watermark was successfully unset."
