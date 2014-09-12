7b9858950a2f4d46400b659106beed9579b222a1
Andy Diamondstein <adiamondstein@google.com>
2014-09-12 12:39:46 -0700

    "Updating samples to reflect recent changes."

--- javascript/auth.js
+++ javascript/auth.js
@@ -29,7 +29,7 @@ function checkAuth() {
 
 // Handle the result of a gapi.auth.authorize() call.
 function handleAuthResult(authResult) {
-  if (authResult) {
+  if (authResult && !authResult.error) {
     // Authorization was successful. Hide authorization prompts and show
     // content that should be visible after authorization succeeds.
     $('.pre-auth').hide();
