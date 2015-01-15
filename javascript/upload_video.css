1e28d18897a15722d171f031ce5b888a237e49fa
Andy Diamondstein <adiamondstein@google.com>
2015-01-15 10:05:53 -0800

    "Added sample: javascript/upload_video.css"

---
+++ javascript/upload_video.css
@@ -0,0 +1,49 @@
+#channel-image {
+  width: 2em;
+  height: 2em;
+  vertical-align: middle;
+}
+
+#channel-name {
+  margin-left: 0.2em;
+  margin-right: 0.2em;
+}
+
+#disclaimer {
+  font-size: 0.75em;
+  color: #aeaeae;
+  max-width: 350px;
+}
+
+body {
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.5em;
+}
+
+.post-sign-in {
+  display: none;
+}
+
+.during-upload {
+  display: none;
+}
+
+.post-upload {
+  display: none;
+}
+
+label {
+  display: block;
+}
+
+input[type="text"], textarea, progress {
+  font-size: 0.75em;
+  width: 15em;
+  margin-bottom: 1em;
+  padding: 0.5em;
+  font-family: "Open Sans", sans-serif;
+}
+
+textarea {
+  height: 7em;
+}
