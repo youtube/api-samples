e3e1998a01eb1542899d1f5588d0e9f8778814d8
Landon Wilkins <landonwilkins@gmail.com>
2015-07-03 18:21:23 -0600

    Update upload_video.rb

--- ruby/upload_video.rb
+++ ruby/upload_video.rb
@@ -9,7 +9,7 @@ require 'google/api_client/auth/installed_app'
 gem 'trollop', '~> 2.1.2'
 require 'trollop'
 gem 'httpclient', '~> 2.6.0'
-Faraday.default_adapter = :httpclient
+Faraday.default_adapter = :excon
 
 # A limited OAuth 2 access scope that allows for uploading files, but not other
 # types of account access.
