ab1fddd79e74470b0d14a56974d4b0abc27f269f
Andy Diamondstein <adiamondstein@google.com>
2015-10-15 13:04:22 -0400

    "Updating samples to reflect recent changes."

--- javascript/auth.js
+++ javascript/auth.js
@@ -50,7 +50,7 @@ function handleAuthResult(authResult) {
 
 // Load the client interfaces for the YouTube Analytics and Data APIs, which
 // are required to use the Google APIs JS client. More info is available at
-// http://code.google.com/p/google-api-javascript-client/wiki/GettingStarted#Loading_the_Client
+// https://developers.google.com/api-client-library/javascript/dev/dev_jscript#loading-the-client-library-and-the-api
 function loadAPIClientInterfaces() {
   gapi.client.load('youtube', 'v3', function() {
     handleAPILoaded();
