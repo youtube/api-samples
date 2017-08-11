c8a19d23443882b5d05b629b803f158abc358f9c
Andy Diamondstein <adiamondstein@google.com>
2017-08-11 12:57:47 -0400

    Add simple error handler

---
+++ go/errors.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "log"
+)
+
+func handleError(err error, message string) {
+  if message == "" {
+    message = "Error making API call"
+  }
+  if err != nil {
+    log.Fatalf(message + ": %v", err.Error())
+  }
+}
