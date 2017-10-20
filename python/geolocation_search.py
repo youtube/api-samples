38651dec50dcc425196f7708ca0206dffea51bae
Andy Diamondstein <adiamondstein@google.com>
2017-10-20 12:36:47 -0400

    Remove oauth2client usages
    Remove usage of oauth2client library, which is now deprecated.

--- python/geolocation_search.py
+++ python/geolocation_search.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from oauth2client.tools import argparser
+# This sample executes a search request for the specified search term.
+# Sample usage:
+#   python geolocation_search.py --q=surfing --location-"37.42307,-122.08427" --location-radius=50km --max-results=10
+# NOTE: To use the sample, you must provide a developer key obtained
+#       in the Google APIs Console. Search for "REPLACE_ME" in this code
+#       to find the correct place to provide that key..
+
+import argparse
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "REPLACE_ME"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+DEVELOPER_KEY = 'REPLACE_ME'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 def youtube_search(options):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
@@ -21,19 +29,19 @@ def youtube_search(options):
   # query term.
   search_response = youtube.search().list(
     q=options.q,
-    type="video",
+    type='video',
     location=options.location,
     locationRadius=options.location_radius,
-    part="id,snippet",
+    part='id,snippet',
     maxResults=options.max_results
   ).execute()
 
   search_videos = []
 
   # Merge video ids
-  for search_result in search_response.get("items", []):
-    search_videos.append(search_result["id"]["videoId"])
-  video_ids = ",".join(search_videos)
+  for search_result in search_response.get('items', []):
+    search_videos.append(search_result['id']['videoId'])
+  video_ids = ','.join(search_videos)
 
   # Call the videos.list method to retrieve location details for each video.
   video_response = youtube.videos().list(
@@ -44,23 +52,23 @@ def youtube_search(options):
   videos = []
 
   # Add each result to the list, and then display the list of matching videos.
-  for video_result in video_response.get("items", []):
-    videos.append("%s, (%s,%s)" % (video_result["snippet"]["title"],
-                              video_result["recordingDetails"]["location"]["latitude"],
-                              video_result["recordingDetails"]["location"]["longitude"]))
+  for video_result in video_response.get('items', []):
+    videos.append('%s, (%s,%s)' % (video_result['snippet']['title'],
+                              video_result['recordingDetails']['location']['latitude'],
+                              video_result['recordingDetails']['location']['longitude']))
 
-  print "Videos:\n", "\n".join(videos), "\n"
+  print 'Videos:\n', '\n'.join(videos), '\n'
 
 
-if __name__ == "__main__":
-  argparser.add_argument("--q", help="Search term", default="Google")
-  argparser.add_argument("--location", help="Location", default="37.42307,-122.08427")
-  argparser.add_argument("--location-radius", help="Location radius", default="5km")
-  argparser.add_argument("--max-results", help="Max results", default=25)
-  args = argparser.parse_args()
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--q', help='Search term', default='Google')
+  parser.add_argument('--location', help='Location', default='37.42307,-122.08427')
+  parser.add_argument('--location-radius', help='Location radius', default='5km')
+  parser.add_argument('--max-results', help='Max results', default=25)
+  args = parser.parse_args()
 
   try:
     youtube_search(args)
   except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
-
+    print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
